b62e228fd595aefed875fff48297a404cd7562e9
John Grassey <johngrassey@gmail.com>
2025-02-02 21:24:33 -0500

    Add Help Text

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -2,16 +2,21 @@ import Card from "./card";
 
 function CardGrid({ pokemon, handleClick }) {
   return (
-    <div className="cardgrid">
-      {pokemon.map((poke) => (
-        <Card
-          key={poke.id}
-          name={poke.name}
-          image={poke.sprites}
-          handleClick={() => handleClick(poke.id)}
-        />
-      ))}
-    </div>
+    <>
+      <div className="cardgrid">
+        {pokemon.map((poke) => (
+          <Card
+            key={poke.id}
+            name={poke.name}
+            image={poke.sprites}
+            handleClick={() => handleClick(poke.id)}
+          />
+        ))}
+      </div>
+      <p className="help">
+        Keep selectiong Pokémon, but don't select the same one twice!
+      </p>
+    </>
   );
 }
 
